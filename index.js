687f5569e6c5af53220c90dc5a587f050cb30346
Yashwanth N <102216969+Yashwanth1811@users.noreply.github.com>
2023-11-13 23:32:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,161 +1,121 @@
-body {
-    text-align: center;
-    font-family: 'Black Ops One', sans-serif;
-    background-color: #b3f6d2;
-    color: #05386B;
-  }
-  
-  h1 {
-    font-size: 4rem;
-    margin:  2% 5% 5%;
-  }
-  
-  p{
-    line-height: 2;
-  }
-  
-  footer {
-    padding: 50px 0 5px;
-  }
-  
-  a {
-    color: #05386B;
-    margin: 10px 20px;
-    display: inline-block;
-    text-decoration: none;
-  }
-  
-  a::after{
-    content: '';
-    width: 0%;
-    height: 2px;
-    background: #000;
-    display: block;
-    margin: auto;
-    transition: 0.5s;
-  }
-  
-  a:hover::after{
-    width: 100%;
-  }
-  
-  .fa {
-    padding-right: 5px;
-  }
-  
-  #outer-circle {
-    background: #05386B;
-    border-radius: 50%;
-    height: 500px;
-    width: 500px;
-    position: relative;
-    border-style: solid;
-    border-width: 10px;
-    margin: auto;
-    margin-top: 60px;
-    box-shadow: 8px 8px 15px 5px #888888;
-  }
-  
-  #inner-circle {
-    position: absolute;
-    background: #FFF;
-    border-radius: 50%;
-    height: 250px;
-    width: 250px;
-    border-style: solid;
-    border-width: 10px;
-    top: 50%;
-    left: 50%;
-    margin: -125px 0px 0px -125px;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-  }
-  
-  #title a {
-    color: blueviolet;
-    font-size: 2rem;
-    margin: 93px auto;
-  }
-  
-  .btn {
-    position: absolute;
-    height: 250px;
-    width: 250px;
-    top: 50%;
-    left: 50%;
-    border-style: solid;
-    border-width: 5px;
-    box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-  }
-  
-  #yellow {
-    border-radius: 250px 0 0 0;
-    -moz-border-radius: 250px 0 0 0;
-    -webkit-border-radius: 250px 0 0 0;
-    background: yellow;
-    margin: -250px 0px 0px -250px;
-  }
-  
-  #red {
-    border-radius: 0 250px 0 0;
-    -moz-border-radius: 0 250px 0 0;
-    -webkit-border-radius: 0 250px 0 0;
-    background: red;
-    margin: -250px 0px 0px 0px;
-  }
-  
-  #blue {
-    border-radius: 0 0 0 250px;
-    -moz-border-radius: 0 0 0 250px;
-    -webkit-border-radius: 0 0 0 250px;
-    background: blue;
-    margin: 0px -250px 0px -250px;
-  }
-  
-  #green {
-    border-radius: 0 0 250px 0;
-    -moz-border-radius: 0 0 250px 0;
-    -webkit-border-radius: 0 0 250px 0;
-    background: green;
-    margin: 0px 0px -250px 0px;
-  }
-  
-  .pressed {
-    box-shadow: 0 0 20px white;
-    background-color: grey;
-  }
-  
-  .game-over {
-    background-color: red;
-    opacity: 0.6;
-  }
+alert("click on 'simonGame' to watch a video on knowing how to play the game.");
 
-  .start {
-    visibility: hidden;
-  }
-  
-  @media (max-width:570px) {
-    h1 {
-      display: inline-block;
-    }
-    a {
-      display: inline-block;
-    }
-  }
+//define 4 colors for 4 buttons
+var btnColors = ["yellow", "red", "blue", "green"];
 
-  @media (max-width:1000px) {
-    .start {
-        visibility: visible;
-        margin-top: 50px;
-        width: 80px;
-        height: 30px;
-        border: solid #05386B;
-        border-radius: 10px;
-        color: blueviolet;
-        font-size: 1rem;
-        font-family: 'Black Ops One', sans-serif;
+//pattern given by the computer
+var gamePattern = [];
+
+//pattern played by the user
+var userPattern = [];
+
+//initially when game is not yet started
+var started = false;
+
+//initial level 0
+var lvl = 0;
+
+$(document).on("dblclick", function () {
+    /* start the game when the webPage is double clicked
+    and when started is not equal to its initial value(logical not) */
+    if (!started) {
+        nextSequence();
+        started = true;
     }
-  }
+});
+
+$(".start").on("touchstart", function () {
+    /* start the game when the webPage is double clicked
+    and when started is not equal to its initial value(logical not) */
+    if (!started) {
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").on("click", function () {
+    //to select clicked button's id
+    var userChosenColor = this.id;
+
+    //to add them into user pattern
+    userPattern.push(userChosenColor);
+
+    //play sound of chosen color
+    playSound(userChosenColor);
+
+    //to flash when btn is pressed
+    flashOnPress(userChosenColor);
+
+    checkAns(userPattern.length - 1);
+});
+
+function nextSequence() {
+    //clear all user clicked pattern so that they start from scratch again
+    userPattern = [];
+
+    //increment level every time nextSequence is called
+    lvl++;
+
+    //to change level every time
+    $("h1").text("level " + lvl);
+
+    //to generate random nos btw 0 to 3
+    var rNo = (Math.floor(Math.random() * 3)) + 1;
+
+    //to allocate colors based on index of random no array
+    var rChosenColor = btnColors[rNo];
+
+    //to add them into computer's pattern
+    gamePattern.push(rChosenColor);
+
+    //to flash the buttons
+    $("#" + rChosenColor).fadeOut(100).fadeIn(100);
+
+    //play sound for chosen color
+    playSound(rChosenColor);
+}
+
+function checkAns(currentLvl) {
+    //check whether user pattern and game pattern are equal
+    if (gamePattern[currentLvl] === userPattern[currentLvl]) {
+        //if yes check sequence of them are equal are not
+        if (gamePattern.length === userPattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        if (window.matchMedia("(max-width: 1000px)").matches) {
+            $("h1").text("Game over , click on Start to restart the game");
+        } else {
+            $("h1").text("Game over , double click to start again");
+        }
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+        reStart();
+    }
+}
+
+//to restart the game
+function reStart() {
+    lvl = 0;
+    gamePattern = [];
+    started = false;
+}
+
+//to play specific sound when specific btn is selected
+function playSound(color) {
+    var x = new Audio("sounds/" + color + ".mp3");
+    x.play();
+}
+
+//to produce flash on chosing color pressed class is added and removed after 100ms
+function flashOnPress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed")
+    }, 100);
+}
